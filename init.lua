6d896dfcb15e94c3899abb7474aad74835bbe63b
giraffecoding3 <alexandermoraitis12@outlook.de>
2025-03-08 17:37:02 +0100

    Nvim Setup Giraffe 3

--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
-require("theprimagen")
+require("giraffe3")
 
---
+++ lua/giraffe3/init.lua
@@ -0,0 +1,12 @@
+require("giraffe3.packer")
+require("giraffe3.remap")
+vim.o.number = true
+vim.o.relativenumber = true
+
+
+
+
+
+
+
+
