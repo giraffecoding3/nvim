b0489b464ec2f878e610e99ec55cd72df0d24f58
giraffecoding3 <alexandermoraitis12@outlook.de>
2025-05-14 14:32:07 +0200

    archlinux pdf szupport

--- lua/giraffe3/init.lua
+++ lua/giraffe3/init.lua
@@ -29,6 +29,23 @@ if vim.fn.has("macunix") == 1 then
 			vim.cmd("bd!") -- Buffer schließen, damit kein "scuffed code" angezeigt wird
 		end,
 	})
+elseif vim.fn.has("linux") == 1 then
+	-- Arch Linux (und andere Linux-Distributionen) spezifische Befehle
+	vim.api.nvim_create_autocmd("BufReadPost", {
+		pattern = "*.pdf",
+		callback = function()
+			local pdf_path = vim.fn.expand("%:p")
+			print("Öffne PDF mit Firefox: " .. pdf_path)
+
+			-- Verwende den Befehl "firefox" zum Öffnen von PDFs unter Linux
+			vim.fn.jobstart({
+				"firefox",
+				pdf_path,
+			}, { detach = true })
+
+			vim.cmd("bd!") -- Buffer schließen
+		end,
+	})
 end
 
 vim.cmd("colorscheme tokyonight")
